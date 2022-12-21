829f32605c2ecb58032e7817d9f9d83bdd83b351
Hernan Visconti <hernan.visconti@bue.edu.ar>
2022-12-21 09:30:02 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+let express =require('express')
+
+const {dirname}=require('path')
+const path=require('path')
+let app=express();
+
+app.use(express.static(path.resolve(__dirname,'./public')))
+app.listen(3000,()=>{
+    console.log('servidor iniciado')
+})
+app.get('/',(req,res)=>{
+    let htmlPath=path.resolve(__dirname,'./views/index.html')
+    res.sendFile(htmlPath)
+})
