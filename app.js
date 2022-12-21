a099a983600c189576b14453e960866a40b0d5cc
Hernan Visconti <hernan.visconti@bue.edu.ar>
2022-12-21 09:34:06 -0300

    agregando render

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ const path=require('path')
 let app=express();
 
 app.use(express.static(path.resolve(__dirname,'./public')))
-app.listen(3000,()=>{
-    console.log('servidor iniciado')
-})
+// app.listen(3000,()=>{
+//     console.log('servidor iniciado')
+// })
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Servidor corriendo");
+  })
 app.get('/',(req,res)=>{
     let htmlPath=path.resolve(__dirname,'./views/index.html')
     res.sendFile(htmlPath)
